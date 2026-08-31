71488e8c9d9a764fd40372db18f2b4c6ba35d0a2
samaelbakry <samaabakry68@gmail.com>
2026-08-31 21:50:08 +0300

    feat: refactor registration flow by creating Register component and updating RegisterPage for build

--- app/(auth)/register/page.tsx
+++ app/(auth)/register/page.tsx
@@ -1,23 +1,10 @@
-"use client"
-import RegisterForm from "@/components/ui/authUI/RegisterForm";
-import AuthLayout from "@/components/ui/authUI/AuthLayout";
-import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
+import Register from "@/components/ui/authUI/Register";
 
-export default function Register() {
-  const searchParams = useSearchParams()
-
-  const role = searchParams.get("role");
-
-  if(role !== "employer" && role !== "job_seeker"){
-    return console.log("Error from register page invalid role")
-  }
-
+export default function RegisterPage() {
   return (
-    <AuthLayout
-      title="Create your account"
-      subtitle="Start tracking applications and organize your job search today."
-    >
-      <RegisterForm role={role} />
-    </AuthLayout>
+    <Suspense fallback={<p>Loading...</p>}>
+      <Register />
+    </Suspense>
   );
 }
---
+++ components/ui/authUI/Register.tsx
@@ -0,0 +1,28 @@
+"use client";
+
+import RegisterForm from "@/components/ui/authUI/RegisterForm";
+import AuthLayout from "@/components/ui/authUI/AuthLayout";
+import { useSearchParams } from "next/navigation";
+
+export default function Register() {
+  const searchParams = useSearchParams();
+
+  const role = searchParams.get("role");
+
+  if (role !== "employer" && role !== "job_seeker") {
+    return (
+      <div className="flex min-h-screen items-center justify-center">
+        Invalid role
+      </div>
+    );
+  }
+
+  return (
+    <AuthLayout
+      title="Create your account"
+      subtitle="Start tracking applications and organize your job search today."
+    >
+      <RegisterForm role={role} />
+    </AuthLayout>
+  );
+}
